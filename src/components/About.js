313762dd4c19051a3eac0f5e6d1f26dbdb874124
Oliver Hole Ekelund <oliver.hole.ekelund@hotmail.no>
2024-09-27 22:57:33 +0200

    Changed about me section

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,14 @@ const About = () => {
     <section id="about" className="mb-16">
       <h2 className="text-3xl font-bold mb-4">About Me</h2>
       <p className="text-gray-700">
-        I'm a 3rd year MSc student in Cybernetics Robotics at NTNU, passionate about automation and robotics. 
-        With experience in leading technical organizations and managing IT projects, I'm always seeking to 
-        improve my professional and innovative problem-solving skills.
+        <ul>
+          <li>3rd-year MSc student in Cybernetics and Robotics at NTNU.</li>
+          <li>Intern experience as an Automation Engineer and mentor in the Schibsted Connect program.</li>
+          <li>Leadership experience from managing one of NTNU’s technical organizations, overseeing a team of 40+ students.</li>
+          <li>Active in extracurriculars, including Head of IT at Nettverksdagene and class representative for the Cybernetics Robotics Class of 2022.</li>
+          <li>Described by peers and superiors as sociable, dedicated, and hard-working, always striving to improve professionally and solve problems innovatively.</li>
+          <li>Motivated by engaging projects, new challenges, and contributing to others’ success, with a focus on achieving measurable results in my work.</li>
+        </ul>
       </p>
     </section>
   );
