79ad3af53fc5da2bfc5be5adf0bcbdf32e93aad0
Oliver Hole Ekelund <oliver.hole.ekelund@hotmail.no>
2024-09-28 00:28:28 +0200

    removed skills in experience section

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,10 @@ const About = () => {
         <ul>
           <li>3rd-year MSc student in Cybernetics and Robotics at NTNU.</li>
           <li>Intern experience as an Automation Engineer and mentor in the Schibsted Connect program.</li>
-          <li>Leadership experience from managing one of NTNU’s technical organizations, overseeing a team of 40+ students.</li>
+          <li>Experience managing Ascend NTNU, one of NTNU’s leading technical organizations, overseeing a team of 46 students at the forefront of autonomous drone technology.</li>
           <li>Active in extracurriculars, including Head of IT at Nettverksdagene and class representative for the Cybernetics Robotics Class of 2022.</li>
-          <li>Described by peers and superiors as sociable, dedicated, and hard-working, always striving to improve professionally and solve problems innovatively.</li>
-          <li>Motivated by engaging projects, new challenges, and contributing to others’ success, with a focus on achieving measurable results in my work.</li>
+          <li>Feedback from superiors and fellow students is that I am a sociable, dedicated, and hard-working individual, always seeking to improve my professional skills and innovative problem-solving abilities.</li>
+          <li>Energized by projects that spark my interest, I thrive on facing new challenges, value supporting others' success, and take pride in achieving measurable results in my work.</li>
         </ul>
       </p>
     </section>
