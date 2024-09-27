57d083f2fc604fe1d8d0802f947a9620fcb20a6f
Oliver Hole Ekelund <oliver.hole.ekelund@hotmail.no>
2024-09-27 22:39:26 +0200

    added fade in when loading site

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Hero from './components/Hero';
 import Navigation from './components/Navigation';
 import About from './components/About';
@@ -18,11 +18,13 @@ const Portfolio = () => {
     <div className="bg-gray-100 text-gray-900 min-h-screen">
       <Hero />
       <Navigation />
-      <div className="max-w-4xl mx-auto py-12 px-8"
+      <div
+        className="max-w-4xl mx-auto py-12 px-8"
         style={{
           opacity: isVisible ? 1 : 0,
           transition: 'opacity 0.5s ease-in-out',
-        }}>
+        }}
+      >
         <section id="about"><About /></section>
         <section id="experience"><Experience /></section>
         <section id="education"><Education /></section>
