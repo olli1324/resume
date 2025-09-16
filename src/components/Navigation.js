3a4fb89402c6b75daae4dd996df9044922c1cbb9
Oliver Hole Ekelund <oliver.hole.ekelund@hotmail.no>
2025-09-16 09:57:11 +0200

    inline cmt

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -25,10 +25,10 @@ const Navigation = () => {
     <nav className="sticky top-0 bg-gray-100 z-10">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          {/* Mobile menu - left aligned */}
+          {/* Mobile menu */}
           <div className="w-10 h-10" />
           
-          {/* Desktop menu - centered */}
+          {/* Desktop menu */}
           <div className="hidden md:block">
             <div className="flex items-baseline space-x-4">
               {navItems.map((item) => (
@@ -43,9 +43,9 @@ const Navigation = () => {
             </div>
           </div>
           
-          {/* Download button - right aligned */}
+          {/* download button */}
           <div className="hidden md:block">
-            {/* Download button temporarily commented out
+            {/*
             <button
               onClick={handleDownload}
               className="w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center hover:bg-gray-300 transition-colors"
